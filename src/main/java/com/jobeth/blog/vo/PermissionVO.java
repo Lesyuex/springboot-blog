3b62812b6223ab6f4ae951c164597b8f4ec1075e
JyrpoKoo <13076652356@163.com>
2020-08-14 17:43:01 +0800

    update

---
+++ src/main/java/com/jobeth/blog/vo/PermissionVO.java
@@ -0,0 +1,11 @@
+package com.jobeth.blog.vo;
+
+/**
+ * desc
+ *
+ * @author JyrpoKoo
+ * @version [版本号 2020/8/14]
+ * @date Created by IntelliJ IDEA in 2020/8/14 17:24
+ */
+public class PermissionVO {
+}
